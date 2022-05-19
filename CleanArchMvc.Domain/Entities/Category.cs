f5d714496ce3e32f4c60013147ab40a8bcd68dbe
Anderson Jardim <andersonjardim@gmail.com>
2022-05-19 13:51:25 -0300

    Aula 28 - Parte 2 - Modelo de domínio anêmico - Chave estrangeria.

--- CleanArchMvc.Domain/Entities/Category.cs
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -1,8 +1,11 @@
-﻿namespace CleanArchMvc.Domain.Entities
+﻿using System.Collections.Generic;
+
+namespace CleanArchMvc.Domain.Entities
 {
     public class Category
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Product> Products { get; set; }
     }
 }
--- CleanArchMvc.Domain/Entities/Product.cs
+++ CleanArchMvc.Domain/Entities/Product.cs
@@ -8,5 +8,7 @@
         public decimal Price { get; set; }
         public int Stock { get; set; }
         public string Image{ get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
     }
 }
