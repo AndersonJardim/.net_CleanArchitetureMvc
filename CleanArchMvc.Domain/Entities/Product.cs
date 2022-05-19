2d0c2f8a947621c65b1885510a4e04f1fa332a23
Anderson Jardim <andersonjardim@gmail.com>
2022-05-19 13:22:46 -0300

    Aula 28 - Parte 1 - Modelo de domínio anêmico
    Criados as 3 pastas:
    md Entities
    md Interfaces
    md Validation
    
    Criado duas classes
    Category.cs
    Product.cs
    
    * Modelo de domínio anêmico, não tem lógica nem validação.
    * Geralmente você vai encontrar muitos exemplos onde as classes de um modelo de domínio são definidos como um modelo anêmico.
    Isso é expresso da seguinte maneira o modelo anêmico é expresso por classes que armazenam apenas dados, onde define apenas propriedades como conjunto de Get e Set sem nenhuma lógica ou validação.
    
    * E esse aqui é um modelo de Domínio Anêmico.
    Não tem lógica não tem validação.
    Essa classe não tem um construtor parametrizado.
    Essa classe pode ser estanciado por qualquer um do projeto e também ela pode ter os seus valores atribuídos usando o Set Público e o Get público a propriedade Name e ID.
    Ou seja meu modelo de domínio definiria dessa maneira está totalmente aberto e não está isolado.

---
+++ CleanArchMvc.Domain/Entities/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace CleanArchMvc.Domain.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public string Image{ get; set; }
+    }
+}
