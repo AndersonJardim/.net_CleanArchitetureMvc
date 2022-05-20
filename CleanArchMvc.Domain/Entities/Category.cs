a4ec93be5c36ef30cfa07f2cdffca9c5a96e1ee0
Anderson Jardim <andersonjardim@gmail.com>
2022-05-20 00:12:14 -0300

    Aula 29 - Enriquecendo o modelo de domínio criado - I

--- CleanArchMvc.Domain/Entities/Category.cs
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -1,11 +1,38 @@
-﻿using System.Collections.Generic;
+﻿using CleanArchMvc.Domain.Validation;
+using System;
+using System.Collections.Generic;
 
 namespace CleanArchMvc.Domain.Entities
 {
-    public class Category
+    public sealed class Category
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+
+        public Category(string name)
+        {
+            //Name = name;            //Name = name ?? throw new ArgumentException("name is invalid");
+            ValidateDomain(name);
+        }
+
+        public Category(int id, string name)
+        {
+            //Id = id;
+            //Name = name;
+            DomainExceptionValidation.When(Id < 0, "Invalid Id value.");
+            Id = id;
+            ValidateDomain(name);
+        }
+
         public ICollection<Product> Products { get; set; }
+
+        private void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Invalid name. Name is required.");
+
+            DomainExceptionValidation.When(name.Length < 3, "Invalid name.Name minimum 3 charecters.");
+
+            Name = name;
+        }
     }
 }
