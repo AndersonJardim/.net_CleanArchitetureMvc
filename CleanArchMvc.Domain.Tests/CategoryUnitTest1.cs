104ed15c3d99a902277fa418139f32faace6d60a
Anderson Jardim <andersonjardim@gmail.com>
2022-06-08 00:28:45 -0300

    Adicionando testes unitários - Parte 3.

--- CleanArchMvc.Domain.Tests/CategoryUnitTest1.cs
+++ CleanArchMvc.Domain.Tests/CategoryUnitTest1.cs
@@ -14,5 +14,40 @@ namespace CleanArchMvc.Domain.Tests
             action.Should()
                  .NotThrow<CleanArchMvc.Domain.Validation.DomainExceptionValidation>();
         }
+
+        [Fact]
+        public void CreateCategory_NegativeIdValue_DomainExceptionInvalidId()
+        {
+            Action action = () => new Category(-1, "Category Name");
+            action.Should()
+                .Throw<CleanArchMvc.Domain.Validation.DomainExceptionValidation>()
+                 .WithMessage("Invalid Id value.");
+        }
+
+        [Fact]
+        public void CreateCategory_ShortNameValue_DomainExceptionShortName()
+        {
+            Action action = () => new Category(1, "Ca");
+            action.Should()
+                .Throw<CleanArchMvc.Domain.Validation.DomainExceptionValidation>()
+                   .WithMessage("Invalid name, too short, minimum 3 characters");
+        }
+
+        [Fact]
+        public void CreateCategory_MissingNameValue_DomainExceptionRequiredName()
+        {
+            Action action = () => new Category(1, "");
+            action.Should()
+                .Throw<CleanArchMvc.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid name.Name is required");
+        }
+
+        [Fact]
+        public void CreateCategory_WithNullNameValue_DomainExceptionInvalidName()
+        {
+            Action action = () => new Category(1, null);
+            action.Should()
+                .Throw<CleanArchMvc.Domain.Validation.DomainExceptionValidation>();
+        }
     }
 }
--- CleanArchMvc.Domain/Entities/Category.cs
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -1,4 +1,5 @@
 ﻿using CleanArchMvc.Domain.Validation;
+using System;
 using System.Collections.Generic;
 
 namespace CleanArchMvc.Domain.Entities
@@ -9,30 +10,29 @@ namespace CleanArchMvc.Domain.Entities
 
         public Category(string name)
         {
-            //Name = name;            //Name = name ?? throw new ArgumentException("name is invalid");
             ValidateDomain(name);
         }
 
         public Category(int id, string name)
         {
-            //Id = id;
-            //Name = name;
-            DomainExceptionValidation.When(Id < 0, "Invalid Id value.");
+            DomainExceptionValidation.When(id < 0, "Invalid Id value.");
             Id = id;
             ValidateDomain(name);
         }
+
         public void Update(string name)
         {
             ValidateDomain(name);
         }
-
         public ICollection<Product> Products { get; set; }
 
         private void ValidateDomain(string name)
         {
-            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Invalid name. Name is required.");
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name),
+                "Invalid name.Name is required");
 
-            DomainExceptionValidation.When(name.Length < 3, "Invalid name.Name minimum 3 charecters.");
+            DomainExceptionValidation.When(name.Length < 3,
+               "Invalid name, too short, minimum 3 characters");
 
             Name = name;
         }
