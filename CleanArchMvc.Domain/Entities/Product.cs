1fb339e14df94b7068d3b94b0aebffc59fc155c3
Anderson Jardim <andersonjardim@gmail.com>
2022-06-08 01:30:15 -0300

    Adicionando testes unitários - ProductUnitTest1.cs -  Parte 2.

--- CleanArchMvc.Domain/Entities/Product.cs
+++ CleanArchMvc.Domain/Entities/Product.cs
@@ -30,21 +30,21 @@ namespace CleanArchMvc.Domain.Entities
 
         private void ValidateDomain(string name, string description, decimal price, int stock, string image)
         {
-            DomainExceptionValidation.When(string.IsNullOrEmpty(name), 
-                "Invalid name. Name is required.");
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name),
+                "Invalid name. Name is required");
 
             DomainExceptionValidation.When(name.Length < 3,
-                "Invalid name, too short, minimum 3 characters.");
+                "Invalid name, too short, minimum 3 characters");
 
             DomainExceptionValidation.When(string.IsNullOrEmpty(description),
                 "Invalid description. Description is required");
 
             DomainExceptionValidation.When(description.Length < 5,
-                "Invalid description, too short, minimum 5 characters.");
+                "Invalid description, too short, minimum 5 characters");
 
-            DomainExceptionValidation.When(price < 0, "Invalid price value.");
+            DomainExceptionValidation.When(price < 0, "Invalid price value");
 
-            DomainExceptionValidation.When(stock < 0, "Invalid stock value.");
+            DomainExceptionValidation.When(stock < 0, "Invalid stock value");
 
             DomainExceptionValidation.When(image.Length > 250,
                 "Invalid image name, too long, maximum 250 characters");
