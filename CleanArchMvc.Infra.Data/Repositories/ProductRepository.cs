291ad97a9489c197a58da65fa2935f0ae3ef786c
Anderson Jardim <andersonjardim@gmail.com>
2022-06-22 01:25:22 -0300

    40. Implementando o repositório - ProductRepository (Parte 1 - Crindo ProductRepository)

---
+++ CleanArchMvc.Infra.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,51 @@
+﻿using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using CleanArchMvc.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Infra.Data.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        ApplicationDbContext _productContext;
+
+        public ProductRepository(ApplicationDbContext context)
+        {
+            _productContext = context;
+        }
+
+        public Task<Product> CreateAsync(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetByIdAsync(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetProductCategoryAsync(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetProductsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> RemoveAsync(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> UpdateAsync(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
