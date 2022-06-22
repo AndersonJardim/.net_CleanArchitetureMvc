c24573b0321d14512b46986cd6694b8c41a22a79
Anderson Jardim <andersonjardim@gmail.com>
2022-06-22 01:17:14 -0300

    39. Implementando o Repositório - CategoryRepository (Parte 1 - Criando CategoryRepository) * Correção ortográfica na interface: GetCatories para GetCategories
    namespace CleanArchMvc.Infra.Data.Repositories
    {
        public class CategoryRepository : ICategoryRepository
        {
           //
        }
    }

---
+++ CleanArchMvc.Infra.Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,47 @@
+﻿using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using CleanArchMvc.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Infra.Data.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        ApplicationDbContext _categoryContext;
+
+        //ctor //atalho para inserir o contrutor automáticamente
+        public CategoryRepository(ApplicationDbContext context)
+        {
+            _categoryContext = context;
+        }
+
+        public Task<Category> Create(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Category> GetById(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Category>> GetCategories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Category> Remove(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Category> Update(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
