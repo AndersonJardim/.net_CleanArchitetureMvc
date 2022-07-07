58553601955b3ecf93627c4170046ad0a2013667
Anderson Jardim <andersonjardim@gmail.com>
2022-07-06 21:46:41 -0300

    44. Aplicando o Migrations do EF Core (Parte 2)
    add-migration SeedProducts

---
+++ CleanArchMvc.Infra.Data/Migrations/20220707004218_SeedProducts.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CleanArchMvc.Infra.Data.Migrations
+{
+    public partial class SeedProducts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
